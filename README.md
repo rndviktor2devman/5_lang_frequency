cf5065842759f628e3c483b8e8dd91c1ff553e90
rndviktor <Vitek_belsutovec@mail.ru>
2016-09-29 00:39:50 +0300

    implemented

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 5_lang_frequency
+
+Run script: type ' python3.5 lang_frequency.py "path_to_txt_file" '
+Neccessary parameter - path to txt file with any text. Without it exception will be thrown.
+
+Script parses whole text, and prints out 10 most frequent word in the text(lower case, punctuation marks are not included)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,29 @@
-
+import re, string, sys
+from itertools import islice
 
 def load_data(filepath):
-    pass
+	with open(filepath) as data_file:
+		splitted_text = re.findall(r"[\w']+", data_file.read())
+	return splitted_text
 
 
 def get_most_frequent_words(text):
-    pass
+	frequencies_dictionary = {}
+	for word in text:
+		lower_word = word.lower()
+		if lower_word in frequencies_dictionary.keys():
+			frequencies_dictionary[lower_word] += 1
+		else:
+			frequencies_dictionary[lower_word] = 1
+
+	for (word, frequency) in islice(reversed(sorted(frequencies_dictionary.items(), key=lambda x:x[1])), 10):
+		print("the word '{}' was found {} times in the text".format(word, frequency))
 
 
 if __name__ == '__main__':
-    pass
+	input_parameter = sys.argv[1]
+	if input_parameter.lower().endswith('.txt'):
+		solid_text = load_data(input_parameter)
+		get_most_frequent_words(solid_text)
+	else:
+		raise NameError('Wrong file type!')
